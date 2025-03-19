91b1854ae781a87f5816e663decc697e0193a52e
Anand <87418392+sssvt-anand@users.noreply.github.com>
2025-03-19 07:16:50 +0530

    Update SecurityConfig.java

--- src/main/java/com/room/app/config/SecurityConfig.java
+++ src/main/java/com/room/app/config/SecurityConfig.java
@@ -94,8 +94,8 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-				"https://react-fornend-git-master-anands-projects-607fcd69.vercel.app",
-				"https://react-fornend-cb2a0vajx-anands-projects-607fcd69.vercel.app", "https://app.roomtracker.fun"));
+				"https://room-react-anands-projects-607fcd69.vercel.app",
+				"https://room-react-agazw6jv8-anands-projects-607fcd69.vercel.app", "https://app.roomtracker.fun"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
